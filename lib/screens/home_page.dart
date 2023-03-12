26d39363ed8234f4ed4f639e0901e8e38bf8a9e0
DELL <anjugupta7060@gmail.com>
2023-03-12 11:26:02 +0530

    Range Selector

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,6 +10,52 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Select Range"),
+      ),
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RangeSelectorTextFormField(
+                lableText: "Minimum",
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 12.0),
+                child: RangeSelectorTextFormField(lableText: "Maximum"),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.arrow_forward),
+      ),
+    );
+  }
+}
+
+class RangeSelectorTextFormField extends StatelessWidget {
+  RangeSelectorTextFormField({
+    super.key,
+    required this.lableText,
+  });
+  String lableText;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: TextInputType.numberWithOptions(
+        decimal: false,
+        signed: true,
+      ),
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: lableText,
+      ),
+    );
   }
 }
